b43a314e820276947fa9e74b6ab2daf6c54ccf49
Henri Halenius <henri.halenius@gmail.com>
2025-10-20 18:54:13 +0900

    changed styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
         {name: "Profile", link: "/profile"}
     ]
     return(
-         <nav className="sticky top-0 w-full flex justify-center items-center h-25">
-            <ul className="flex items-center text-lg bg-white/20 backdrop-blur-sm rounded-2xl shadow-sm font-mono">
+         <nav className="sticky top-0 w-max mx-auto py-1">
+            <ul className="flex justify-center text-lg w-md bg-white/20 backdrop-blur-sm rounded-2xl shadow-sm font-mono">
                 {links.map((link) => (
                     <a href={link.link} key={link.name}>
                         <li className="hover:bg-gray-100/60 py-3 px-6 rounded-2xl">
